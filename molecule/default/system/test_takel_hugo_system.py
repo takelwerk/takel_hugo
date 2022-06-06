b859293c9898aff13ad523e3f80f05d8d970db34
Stephan <stephan@takelwerk.net>
2022-06-06 13:54:57 +0200

    Update hg repos

--- molecule/default/system/test_takel_hugo_system.py
+++ molecule/default/system/test_takel_hugo_system.py
@@ -8,7 +8,7 @@ def test_takel_hugo_system_hugo_version(host, testvars):
     hugo_version = str(testvars['takel_hugo_hugo_version'])
     hugo_version_output = host.check_output('hugo version')
     hugo_version_search = re.search(
-        r'.*(\d{1,2}\.\d{1,2}\.\d{0,2}).*', hugo_version_output)
+        r'.*(\d{1,2}\.\d{1,3}\.\d{0,2}).*', hugo_version_output)
 
     if hugo_version_search is not None:
         assert hugo_version_search.group(1) == hugo_version
