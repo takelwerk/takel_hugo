c89ebc485dd50cdcee905ae28324d7a955502d40
Stephan <stephan@takelwerk.net>
2023-02-05 21:54:15 +0100

    Update hg repos

--- molecule/default/system/test_takel_hugo_system.py
+++ molecule/default/system/test_takel_hugo_system.py
@@ -1,16 +1,22 @@
 import takeltest
 import re
+import requests
 
 testinfra_hosts = takeltest.hosts()
 
 
 def test_takel_hugo_system_hugo_version(host, testvars):
-    hugo_version = str(testvars['takel_hugo_hugo_version'])
-    hugo_version_output = host.check_output('hugo version')
+    takel_hugo_hugo_version = str(testvars['takel_hugo_hugo_version'])
+    hugo_version_output = \
+        host.check_output('hugo version')
     hugo_version_search = re.search(
-        r'.*(\d{1,2}\.\d{1,3}\.\d{0,2}).*', hugo_version_output)
+        r'.*(v\d{1,2}\.\d{1,3}\.\d{0,2}).*', hugo_version_output)
 
-    if hugo_version_search is not None:
-        assert hugo_version_search.group(1) == hugo_version
-    else:
-        assert False, 'Unable to get hugo version'
+    assert hugo_version_search is not None, 'Unable to get hugo version'
+
+    if takel_hugo_hugo_version == 'latest':
+        url = "https://api.github.com/repos/gohugoio/hugo/releases/latest"
+        response = requests.get(url)
+        hugo_version = response.json()["tag_name"]
+    assert hugo_version_search.group(1) == hugo_version, \
+        'Unable to get hugo version'
